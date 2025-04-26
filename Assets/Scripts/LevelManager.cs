105da31e65cbfd3604e9bc7e750e6611bb061d33
unknown <vova.vevo666@gmail.com>
2025-04-26 21:37:21 +0300

    upd

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager main;
+
+    public Transform startPosition;
+    public Transform[] path;
+
+    private void Awake()
+    {
+        main = this;
+    }
 }
--- Assets/Scripts/MoveControllerEnemy.cs
+++ Assets/Scripts/MoveControllerEnemy.cs
@@ -1,15 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveControllerEnemy : MonoBehaviour
 {
+
+    [SerializeField] private float speedMove;
+
+    private int indexPath = 0;
+    private Transform target;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Start()
     {
-        
+        target = LevelManager.main.path[indexPath];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Vector3.Distance(target.position, transform.position) <= 0.1f)
+        {
+            indexPath++;
+
+            if(indexPath >= LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[indexPath];
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * speedMove;
     }
 }
